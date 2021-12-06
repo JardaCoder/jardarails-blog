52a8e0528b0a774d1b5aa9b9577b4b7c6550cf04
Jardel <jardel.leandro25@gmail.com>
2021-12-05 21:49:00 -0300

    Tela de visualização de post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
+  # get all articles
   def index
     @articles = Article.all
+  end
 
-    
-
+  # get one article by id
+  def show
+    @article = Article.find(params[:id])
   end
 end
